819ce4e14f2049daa4f1e23cfb7079c8eae86d74
ecruos <ecruos@outlook.com>
2020-01-08 11:22:03 +0800

    Update

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const VERSION = 'v0.12.29';
-export const OO_PLUGIN_VERSION = '20.1.8';
+export const OO_PLUGIN_VERSION = '20.1.9';
 export const STORE_PREFIX = 'One';
 
 export const defaultSearches = `多吉 https://www.dogedoge.com/results?q=
