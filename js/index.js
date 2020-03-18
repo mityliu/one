7d7816a6a0e11e36b7d052f73609bd5f1452d8c2
ecruos <ecruos@outlook.com>
2020-03-18 22:12:27 +0800

    Add /play

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 import '../styles/main.scss';
 import { h, render } from 'preact';
+import Router from 'preact-router';
 import Home from './Home';
+import Play from './Play';
 import './stats';
 
 render(<Home />, document.body);
+
+const Main = () => (
+  <Router>
+    <Home path="/" />
+    <Play path="/play" />
+  </Router>
+);
+
+render(<Main />, document.body);
